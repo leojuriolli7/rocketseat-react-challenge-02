1dec0f535123940ffff7473998bd7f20aa74c538
leojuriolli7 <leojuriolli@gmail.com>
2022-02-27 19:09:26 -0300

    Initial commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+interface GenreResponseProps {
+  id: number,
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string,
+}
+
+interface ContentProps {
+  selectedGenre: GenreResponseProps
+}
+
+export function Header({ selectedGenre }: ContentProps) {
+  return(
+    <header>
+      <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+    </header>
+  )
+}
